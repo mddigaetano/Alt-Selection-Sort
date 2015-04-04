88649845ecbad7c8454c157147784cd9e0099845
amedeo <amedeo@amedeo-N250P>
2015-04-04 11:05:39 +0200

    Svolte Consegne

---
+++ main.c
@@ -0,0 +1,44 @@
+//Author: amedeo39
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 5
+
+void SelectionSort(int v[],int n){
+    int i,max,j,scambio;
+    
+    for(i=0;i<n-1;i++){
+        max=i;
+        
+        for(j=i+1;j<n;j++){
+            if(v[j]>v[max]){
+                max=j;
+            }
+        }
+        
+        scambio=v[i];
+        v[i]=v[max];
+        v[max]=scambio;
+    }
+}
+
+int main(int argc, char** argv) {
+    int i,v[N];
+    
+    srand(time(NULL));
+    
+    for(i=0;i<N;i++){
+        v[i]=(rand()%100)+1;
+        printf("Vettore[%d]: %d\n",i,v[i]);
+    }
+    
+    SelectionSort(v,N);
+    
+    for(i=0;i<N;i++){
+        printf("\nVettore[%d]: %d",i,v[i]);
+    }
+
+    return (EXIT_SUCCESS);
+}
